b69dd7625ca032e89319a3b9fcc511531b6d7ac9
Kaneknah <jamesnbaxley@protonmail.com>
2023-02-28 19:32:05 -0700

    Wrote uuid code for create notes.

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 
 ## Description
 
-this project was the creation of a note taker app that specifically utilized Express.js on the back end to save and retrieve data from JSON files (Link to live website). The application utilized router.get, router.put, and router.delete to retrieve and render added notes on the front end. The application utilizes starter code as the front end.
+This project was the creation of a note taker app that specifically utilized Express.js on the back end to save and retrieve data from JSON files (Link to live website). The application utilized router.get, router.put, and router.delete to retrieve and render added notes on the front end. The application utilizes starter code as the front end.
 
 ## Acceptance Criteria
 
@@ -70,6 +70,10 @@ No testing was utilized for this project
 
 -Express.js<br>
 -Node.js<br>
+
+#### Deployment
+
+-Heroku<br>
 
 ### GitHub Link: <https://github.com/Kaneknah/ch11_baxley_note_taker>
 
--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 let { notesArray } = require("../../db/notes.json");
 const { createNote, deleteNote, editNote } = require("../../lib/notes");
+const uuid = require("uuid");
 
 // GET Route for retrieving all the note
 router.get("/notes", (req, res) => {
@@ -8,11 +9,11 @@ router.get("/notes", (req, res) => {
 	res.json(results);
 	console.log(results);
 });
-
+//NEED to add an ID for each created note
 //POST Route for submitting Notes ADD
 router.post("/notes", (req, res) => {
 	console.log("Note Added");
-	const notes = createNote(req.body, notesArray);
+	const notes = createNote(req.body);
 	res.json(notes);
 	console.log(notes);
 });
@@ -23,7 +24,7 @@ router.put("./notes/:id", (req, res) => {
 });
 
 //Deleting Created Notes
-router.delete("/notes", function (req, res) {
+router.delete("/notes/:id", function (req, res) {
 	console.log("Note Deleted");
 	const note = deleteNote(req.params.id, note);
 	res.json(note);
