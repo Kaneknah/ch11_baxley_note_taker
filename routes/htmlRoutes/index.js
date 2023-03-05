b433776175967ce206559db16fa96df0fc20db60
Kaneknah <jamesnbaxley@protonmail.com>
2023-03-05 11:35:16 -0700

    added Comments

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const router = require("express").Router();
 
+//Connects our main page to the HTML source.
 router.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
+//Connects out Notes page to the HTML source
 router.get("/notes", (req, res) => {
 	res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
